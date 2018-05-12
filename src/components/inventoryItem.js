54358633921501f074a1c24aa57111d75d20646d
Quinton Woodmansee <qwoodmansee@capital.edu>
2018-05-12 14:53:48 -0400

    first commit of inventory item component

---
+++ src/components/inventoryItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class InventoryItem extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>Inventory Item Example</div>
+        )
+    }
+}
